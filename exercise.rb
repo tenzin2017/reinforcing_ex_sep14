8d20be8d881a897c942e1b9630d804444cb53b83
tenzin2017 <tenzinleo@gmail.com>
2017-09-15 21:56:03 -0400

    reinforcement on iteration, string-interpolation, hash, array

---
+++ exercise.rb
@@ -0,0 +1,17 @@
+array = [
+{id: "38fj8d900", city: 'Hamilton', events: [{date: '2017-01-01', attendees: 100}, {date: '2016-12-31', attendees: 60}]},
+{id: "39fo837y7", city: 'Toronto', events: [{date: '2017-03-30', attendees: 3000}, {date: '2017-07-07', attendees: 2500}, {date: '2017-02-04', attendees: 900}]},
+{id: "58uj8d800", city: 'Montreal', events: [{date: '2017-08-10', attendees: 250}]},
+{id: "48hn8d900", city: 'Kingston', events: [{ date: '2015-04-16', attendees: 45}]}
+]
+
+array.each do |each_event|
+  puts "#{each_event[:city]}"
+  puts "--------------------"
+  event_date = each_event[:events]
+
+  event_date.each do |detail|
+  puts "Date: #{detail[:date]}, #{detail[:attendees]} people"
+  end
+  puts " "
+end
